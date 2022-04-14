652d952b60def99c262827f9b4d48760f904a014
Don Romaniello <don.romaniello@gmail.com>
2022-04-13 21:29:34 -0400

    Starting to wire the API.

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const port = 3000
 const path = require('path');
+const { db, Cat, Weight, Food } = require('./client/database')
+
 
 app.use(express.static('public'));
 
@@ -9,7 +11,17 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, './public/index.html'))
 })
 
+app.use('/api', async(req, res, next) => {
+  try {
+    const cats = await Cat.findAll()
+    res.send(cats)
+  } catch(err){
+    next(err)
+  }
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+

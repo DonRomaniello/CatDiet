87139cdacfecfdf9e4ac2c9a65ebd31e199d150a
Don Romaniello <don.romaniello@gmail.com>
2022-04-18 17:10:39 -0400

    Working food API calls.

--- server/app.js
+++ server/app.js
@@ -7,6 +7,39 @@ const { db, Cat, Weight, Food } = require('../client/database')
 
 app.use(express.static('public'));
 
+
+app.get('/api/currentfood', async(req, res, next) => {
+  try {
+    const sebFood = await Food.findAll({
+      limit: 1,
+      where: {
+        catId: 1
+      },
+      order: [['date', 'DESC']]
+    })
+
+    const lunFood = await Food.findAll({
+      limit: 1,
+      where: {
+        catId: 2
+      },
+      order: [['date', 'DESC']]
+    }
+    )
+
+    const food = {
+      luni: lunFood,
+      sebastian: sebFood
+    }
+
+
+
+    res.send(food)
+  } catch (err) {
+    next(err)
+  }
+})
+
 app.get('/api', async(req, res, next) => {
   try {
     const cats = await Cat.findAll()

44233653ea94445885df0008884508db3e7c3461
Don Romaniello <don.romaniello@gmail.com>
2022-04-18 16:45:29 -0400

    Working React...

--- src/foodTable.js
+++ src/foodTable.js
@@ -1,7 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react"
 
-ReactDOM.render(
-  <div>Hello, world!</div>,
-  document.getElementById('food') // make sure this is the same as the id of the div in your index.html
-);
+const Food = () => {
+  return (
+    <div>
+      These true kitties.
+    </div>
+  )
+}
+
+export default Food
--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import Food from './foodTable.js'
+
 ReactDOM.render(
-  <div>These cats.</div>,
-  document.getElementById('food') // make sure this is the same as the id of the div in your index.html
+  <Food />,
+  document.getElementById('main') // make sure this is the same as the id of the div in your index.html
 );
+
